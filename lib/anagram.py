5956d808f197b9a1ece41806dc02a9a972dd97d2
AGNESGITAU <agnesgitau40@gmail.com>
2024-05-27 07:09:34 +0300

    Instantiate with word

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def _init_(self,word):
+      self.word = word.lower()
+      pass
+
+    def match(self, possible_anagrams):
+           matches = []
+           for anagram in possible_anagrams:
+               if self.is_anagram(anagram.lower()):
+                   matches.append(anagram)
+                   return matches
+               
+               def is_anagram(self, other_word):
+                   if len(self.word) != len(other_word) or self.word == other_word:
+                       return False
+                   return sorted(self.word) == sorted(other_word)
